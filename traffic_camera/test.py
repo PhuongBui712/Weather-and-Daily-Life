d003f9fd45e515db7a67d7f60b93466dc535361e
HiepTr <118036757+HiepTr@users.noreply.github.com>
2024-03-25 04:24:35 +0700

    Add files via upload

--- traffic_camera/test.py
+++ traffic_camera/test.py
@@ -10,4 +10,5 @@ options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 driver.get("http://giaothong.hochiminhcity.gov.vn/expandcameraplayer/?camId=63ae7cfcbfd3d90017e8f422&camLocation=C%C3%A1ch%20M%E1%BA%A1ng%20Th%C3%A1ng%20T%C3%A1m%20%E2%80%93%20Ph%E1%BA%A1m%20V%C4%83n%20Hai&camMode=camera&videoUrl=https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8")
-print(driver.title)
+driver.set_window_size(1000, 800)
+driver.save_screenshot("Pham Van Hai", "pic.png")

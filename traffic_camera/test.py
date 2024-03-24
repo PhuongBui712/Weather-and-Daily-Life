b4ba5f7b46243f6bc62b4c1c425b2b2bd46e7471
HiepTr <118036757+HiepTr@users.noreply.github.com>
2024-03-25 04:28:08 +0700

    Add files via upload

--- traffic_camera/test.py
+++ traffic_camera/test.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 options = Options()
 options.add_argument('--headless')
@@ -9,6 +10,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
+folder_name = os.path.join("Pham Van Hai", "picture.png")
 driver.get("http://giaothong.hochiminhcity.gov.vn/expandcameraplayer/?camId=63ae7cfcbfd3d90017e8f422&camLocation=C%C3%A1ch%20M%E1%BA%A1ng%20Th%C3%A1ng%20T%C3%A1m%20%E2%80%93%20Ph%E1%BA%A1m%20V%C4%83n%20Hai&camMode=camera&videoUrl=https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8")
 driver.set_window_size(1000, 800)
-driver.save_screenshot("Pham Van Hai", "pic.png")
+driver.save_screenshot(folder_name)
